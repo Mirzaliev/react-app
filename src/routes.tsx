51b80955eb6a90d9c7ed441f5c5a767209e81f40
Мирзалиев Арсен <arsen.mirzaliev@mail.ru>
2022-01-26 17:40:03 +0300

    react-router-dom

---
+++ src/routes.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function Router(): JSX.Element {
+  return (<Switch>
+
+  </Switch>);
+};
